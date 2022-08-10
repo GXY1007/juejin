ff1caa053b007a44fa7d9c3459d41da6d2722e0d
unknown <981332463@qq.com>
2022-08-10 11:15:01 +0800

    添加全局颜色变量

---
+++ src/components/subnav.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="subnav">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "subnav"
+}
+</script>
+
+<style scoped lang="less">
+.subnav{
+  width: 100%;
+  height: 50px;
+  background-color: white;
+  border-top:1px solid @background;
+  box-shadow: 0 8px 5px -5px @borderShadow;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,74 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav></nav>
+    <subnav></subnav>
+    <div class="container">
+      <main>
+        <span class="iconfont icon-xiala1"></span>
+      </main>
+      <aside></aside>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import subnav from "@/components/subnav";
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+</script>
+
+<style lang="less" scoped>
+
+.home{
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  min-height: 100vh;
+  background-color: @background;
+
+  nav{
+    min-height: 30px;
+    background-color: white;
+  }
+
+  .container{
+    flex: 1;
+
+    display: flex;
+    flex-direction: row;
+
+    width: 80%;
+    margin: 0 auto;
+    padding-top: 20px;
+
+    main{
+      flex: 1;
+      background-color: white;
+
+
+    }
+
+    aside{
+      width: 260px;
+      margin-left: 20px;
+      background-color: white;
+
+      //<=960
+      @media screen and (max-width:960px){
+        display: none;
+      }
+
+    }
+  }
+
+  .iconfont{
+    font-size: 22px !important;
+  }
+  .icon-xiala1{
+    color: #F56C6C;
+    &:hover{
+      cursor: pointer;
+    }
   }
 }
-</script>
+
+</style>
